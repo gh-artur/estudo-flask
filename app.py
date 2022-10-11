5f28667d9148cfb8b90578ae1becf56534e50bff
Artur Santos <arturfrancisco86@gmail.com>
2022-10-10 22:18:40 -0300

    teste

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/salve')
+def salve():
+    return 'eai doidão'
 
 if __name__ == '__main__':
     app.run()
